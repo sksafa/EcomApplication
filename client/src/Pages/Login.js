76ae5e1cdf20ef6d72de8c48c332c8c5a64ddf88
saymon-shoab <mdsaymonshoab@gmail.com>
2023-02-22 06:14:01 +0600

    form validation done

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { registerSchema } from "../Components/Schema/FormSchema";
+
+
 
 const Login = () => {
+
+ 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+  // Saving space
+
+  const formSubmitHandler = (data) => {
+    console.log(data);
+  };
+
+// Saving space
+
   return (
 
     <div className="flex flex-col justify-center items-center px-6 pt-8 mx-auto md:h-screen pt:mt-0">
@@ -13,31 +35,65 @@ const Login = () => {
         <div className="p-10 w-full max-w-lg bg-white rounded-2xl shadow-xl shadow-gray-300">
             <div className="space-y-8">
                 <h2 className="text-2xl font-bold text-gray-900">
-                    Sign in to platform
+                    Sign in to IBN
                 </h2>
-                <form className="mt-8 space-y-6" action="#">
+                    <form onSubmit={handleSubmit(formSubmitHandler)} className="mt-8 space-y-6" action="#">
+                    {/* <div className="mt-8 space-y-6" action="#"> */}
                     <div>
                         <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                        <input type="email" name="email" id="email" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="name@company.com" required />
+                        <input
+                          {...register("email")}
+                          type="email"
+                          name="email"
+                          id="email"
+                          // {...register("email")}
+                          className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                          placeholder="name@company.com"
+                        />
                     </div>
+                     {errors.email ? (
+                          <span className=""><p className=' text-red-600 text-sm'>{errors.email.message}</p></span>
+                        ) : (
+                          <></>
+                      )}
                     <div>
                         <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" required />
+                        <input
+                          {...register("password")}
+                          type="password"
+                          name="password"
+                          id="password"
+                          placeholder="••••••••"
+                          className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                        />
                     </div>
+                    {errors.password ? (
+                      <span className="text-red-600"><p className=' text-sm'>{errors.password.message}</p></span>
+                    ) : (
+                      <></>
+                    )}
                     <div className="flex items-start">
                         <div className="flex items-center h-5">
-                        <input id="remember" aria-describedby="remember" name="remember" type="checkbox" className="w-5 h-5  rounded border-gray-300 focus:outline-none focus:ring-0 checked:bg-dark-900" required />
+                        <input
+                         id="remember"
+                         aria-describedby="remember"
+                         name="remember" type="checkbox"
+                         className="w-5 h-5 rounded border-gray-300 focus:outline-none focus:ring-0 checked:bg-dark-900"
+                        />
                         </div>
                         <div className="ml-3 text-sm">
                         <label for="remember" className="font-medium text-gray-900">Remember me</label>
                         </div>
                         <Link to="/forgotpassword" className="ml-auto text-sm text-fuchsia-600 hover:underline">Lost Password?</Link>
                     </div>
-                    <button type="submit" className="py-3 px-5 w-full text-base font-medium text-center text-white bg-gradient-to-br from-blue-500 to-voilet-500 hover:scale-[1.02] shadow-md shadow-gray-300 transition-transform rounded-lg sm:w-auto">Login to your account</button>
+                    <input type="submit" className="py-3 px-5 w-full text-base font-medium text-center text-white bg-gradient-to-br from-blue-500 to-voilet-500 hover:scale-[1.02] shadow-md shadow-gray-300 transition-transform rounded-lg sm:w-auto"/>
+                    {/* <input type="submit" className=' text-base font-medium text-center bg-gradient-to-br' /> */}
                     <div className="text-sm font-medium text-gray-500">
                         Not registered? <Link to="/register" className="text-blue-600 hover:underline">Create account</Link>
                     </div>
-                </form>
+                   {/* </div> */}
+                    </form>
+
             </div>
         </div>
     </div>
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { registerSchema } from "../Components/Schema/FormSchema";
+
 
 const Ragister = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        resolver: yupResolver(registerSchema),
+      });
+      // Saving space
+    
+      const formSubmitHandler = (data) => {
+        console.log(data);
+      };
   return (
 
 <div className="flex flex-col justify-center items-center px-6 pt-8 mx-auto md:h-screen pt:mt-0">
@@ -15,35 +31,87 @@ const Ragister = () => {
             <h2 className="text-2xl font-bold text-gray-900">
                 Create a Free Account
             </h2>
-            <form className="mt-8 space-y-6" action="#">
+            <form onSubmit={handleSubmit(formSubmitHandler)} className="mt-8 space-y-6" action="#">
             <div className="flex -mx-3">
                       <div className="w-1/2 px-3">
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900"> first name</label>
+                        <label for="fitst name" className="block mb-2 text-sm font-medium text-gray-900"> first name</label>
                             <div className="flex">
                                 <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John" />
+                                <input 
+                                type="text"
+                                {...register("firstName")} 
+                                className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" 
+                                placeholder="John"
+                                required
+                                />
                             </div>
+                            
                         </div>
+
                         <div className="w-1/2 px-3">
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900">last name</label>
+                        <label for="last name" className="block mb-2 text-sm font-medium text-gray-900">last name</label>
                             <div className="flex">
                                 <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Smith" />
+                                <input 
+                                type="text" 
+                                {...register("lastName")}
+                                className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" 
+                                placeholder="Smith"
+                                required
+                                />
                             </div>
                         </div>
-                    </div>
+            </div>
+            
                 <div>
                     <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                    <input type="email" name="email" id="email" className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="name@company.com" required />
+                    <input 
+                        {...register("email")}
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" 
+                        placeholder="name@company.com"
+                    />
                 </div>
+                {errors.email ? (
+                          <span className=""><p className=' text-red-600 text-sm'>{errors.email.message}</p></span>
+                        ) : (
+                          <></>
+                      )}
                 <div>
                     <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" required />
+                    <input 
+                        {...register("password")}
+                        type="password"
+                        name="password" 
+                        id="password" 
+                        placeholder="••••••••" 
+                        className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                    />
                 </div>
+                {errors.password ? (
+                    <span className="text-red-600">{errors.password.message}</span>
+                ) : (
+                    <></>
+                )}
                 <div>
                     <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
-                    <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" required />
+                    <input 
+                        {...register("confirmPassword")}
+                        type="password" 
+                        name="confirmPassword" 
+                        id="confirmPasswordd" 
+                        placeholder="••••••••" 
+                        className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" 
+                     />
                 </div>
+                {errors.confirmPassword ? (
+                    <span className="text-red-600">{errors.confirmPassword.message}</span>
+                ) : (
+                    <></>
+                )}
+
                 <div className="flex items-start">
                     <div className="flex items-center h-5">
                         <input id="remember" aria-describedby="remember" name="remember" type="checkbox" className="w-5 h-5 rounded border-gray-300 focus:ring-3 focus:ring-0 checked:bg-dark-900" required />
@@ -52,7 +120,8 @@ const Ragister = () => {
                         <label for="remember" className="font-medium text-gray-900">I accept the <a href="#" className="text-fuchsia-500 hover:underline">Terms and Conditions</a></label>
                     </div>
                 </div>
-                <button type="submit" className="py-3 px-5 w-full text-base font-medium text-center text-white bg-gradient-to-br from-blue-500 to-voilet-500 hover:scale-[1.02] shadow-md shadow-gray-300 transition-transform rounded-lg sm:w-auto">Create account</button>
+                <input type="submit" className="py-3 px-5 w-full text-base font-medium text-center text-white bg-gradient-to-br from-blue-500 to-voilet-500 hover:scale-[1.02] shadow-md shadow-gray-300 transition-transform rounded-lg sm:w-auto"/>
+
                 <div className="text-sm font-medium text-gray-500">
                     Already have an account? <Link to="/login"  className="text-blue-500 hover:underline">Login here</Link>
                 </div>
