43bee40a0e66ff6ce9cc5deabbf99403fa9b033b
safawat hossain <sifakatali@gmail.com>
2023-02-28 20:47:11 +0600

    ss

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 const moragan = require('morgan')
 const dotenv = require('dotenv');
 const { default: mongoose } = require('mongoose');
+mongoose.set('strictQuery', true);
 
 // dot env config
 dotenv.config();
@@ -13,14 +14,13 @@ const app = express()
 app.use(express.json())
 app.use(moragan('dev'))
 // database...
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(()=> console.log("database is connected"))
 .catch((error)=>console.log("db connection error", error))
 // routes
-console.log("hudai",process.env.MONGO_URI)
 app.get('/',(req,res)=>{
    res.status(200).send({
     message: 'app is running'
@@ -31,7 +31,7 @@ const port = process.env.PORT || 8080;
 // listen port
 app.listen(port,()=>{
     console.log(
-        `server running in ${process.env.NODE_MODE} mode on port ${process.env.PORT} `
+        `server running in  mode on port ${process.env.PORT} `
         .bgCyan.white
         )
 })
