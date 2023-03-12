9318871483680537ecf2bc1b094420d22d15bb68
safawat hossain <sifakatali@gmail.com>
2023-03-13 00:12:19 +0600

    banner half done

---
+++ client/src/Components/Banner/Banner.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import hero from '../../Image/hero.png'
+const Banner = () => {
+    return (
+        <div className="container mx-auto">
+            <div className="grid grid-cols-2 gap-4 ">
+                <div className=' grid content-center descriptionArea'>
+                    <h2 className="mb-5 text-4xl tracking-tight  text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Find Best Doctor</h2>
+                    <p className="mb-6 text-lg font-normal text-gray-500 lg:text-lg sm:px-16 xl:px-0 dark:text-gray-400">Great doctor if you need your family member to get immediate assistance, emergency treatment or a simple consultation..</p>
+
+                    <form>
+                        <label for="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                        <div className="relative">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                            </div>
+                            <input type="search" id="search" className="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+                            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        </div>
+                    </form>
+                    <p className="mt-6 text-lg font-normal text-gray-500 lg:text-lg sm:px-16 xl:px-0 dark:text-gray-400"><span className=''>Note: </span> Please search best doctors here</p>
+                </div>
+                <div className='imageArea'>
+                    <img src={hero} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navber from '../Components/Navber/Navber'
 import Footer from '../Components/Footer/Footer';
+import Banner from '../Components/Banner/Banner';
 
 
 const Home = () => {
   return (
     <div>
       <Navber />
+      <Banner/>
       <Footer/>
     </div>
   )
